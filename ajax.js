484c8b3a6c29a1d37e3dee39f9e2fed8c1ad46b6
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-02 11:48:08 +0200

    ajax.js

--- ajax.js
+++ ajax.js
@@ -4,10 +4,10 @@
 // Påbörja en hämtning
 var request = new XMLHttpRequest();
 
-request.onreadystatechange = function(req, res) {
+request.onreadystatechange = function() {
 	console.log("Någonting händer");
     // I var res (response) finns det information
-    console.log("Svaret (response) är " + res.responseText);
+    console.log("Svaret (response) är " + request.responseText);
 }
 
 request.open("GET","http://mardby.se/AJK15G/lorem_text.php");
