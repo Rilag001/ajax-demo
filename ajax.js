4d6b41f6277d5ce0ccf6de3f0c595047513031df
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-02 11:46:41 +0200

    ajax.js new http

--- ajax.js
+++ ajax.js
@@ -7,7 +7,7 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function(req, res) {
 	console.log("Någonting händer");
     // I var res (response) finns det information
-    console.log("Svaret (response) är " + response.responseText);
+    console.log("Svaret (response) är " + res.responseText);
 }
 
 request.open("GET","http://mardby.se/AJK15G/lorem_text.php");
